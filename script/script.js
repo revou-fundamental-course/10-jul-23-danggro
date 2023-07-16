992a246c917858a25f50c44fc716c07d5a6cdcf0
danggro <anangdwin55@gmail.com>
2023-07-16 20:33:31 +0700

    add function back to calculation, add image, style, response female bmi display

--- script/script.js
+++ script/script.js
@@ -1,9 +1,23 @@
 function hitungBMI() {
+  const jenisKelamin = document.getElementsByName("jenis-kelamin");
   let beratBadan = document.getElementById("berat-badan");
   let usia = document.getElementById("usia");
   let tinggiBadan = document.getElementById("tinggi-badan");
   let valueBMI = document.getElementById("value-bmi");
   let textSaran = document.getElementById("text-saran");
+  const imageMale = document.getElementById("img-bmi-display-male");
+  const imageFemale = document.getElementById("img-bmi-display-female");
+
+  for (let i = 0; i < jenisKelamin.length; i++) {
+    if (jenisKelamin[i].checked) {
+      if (jenisKelamin[i].value == "laki-laki") {
+        imageMale.style.display = "flex";
+      } else {
+        imageFemale.style.display = "flex";
+      }
+    }
+  }
+
   let dataTextSaran = {};
   let result = (beratBadan.value / (tinggiBadan.value / 100) ** 2).toFixed(1);
 
@@ -32,31 +46,46 @@ function hitungBMI() {
 }
 
 function kekuranganBeratBadan(valueBMI, textSaran, dataTextSaran) {
-  const containerImage = document.getElementById("kekurangan-bb");
-  containerImage.style.opacity = "100%";
+  const containerImage = document.getElementsByClassName("kekurangan-bb");
+  containerImage[0].style.opacity = "100%";
+  containerImage[1].style.opacity = "100%";
   valueBMI.style.color = "var(--underweight)";
   textSaran.innerHTML = dataTextSaran["kekurangan-bb"];
 }
 
 function normal(valueBMI, textSaran, dataTextSaran) {
-  const containerImage = document.getElementById("normal");
-  containerImage.style.opacity = "100%";
+  const containerImage = document.getElementsByClassName("normal");
+  containerImage[0].style.opacity = "100%";
+  containerImage[1].style.opacity = "100%";
   valueBMI.style.color = "var(--normal)";
   textSaran.innerHTML = dataTextSaran["normal"];
 }
 
 function kelebihanBeratBadan(valueBMI, textSaran, dataTextSaran) {
-  const containerImage = document.getElementById("kelebihan-bb");
-  containerImage.style.opacity = "100%";
+  const containerImage = document.getElementsByClassName("kelebihan-bb");
+  containerImage[0].style.opacity = "100%";
+  containerImage[1].style.opacity = "100%";
   valueBMI.style.color = "var(--overweight)";
   textSaran.innerHTML = dataTextSaran["kelebihan-bb"];
 }
 
 function obesitas(valueBMI, textSaran, dataTextSaran) {
-  const containerImage = document.getElementById("obesitas");
-  containerImage.style.opacity = "100%";
+  const containerImage = document.getElementsByClassName("obesitas");
+  containerImage[0].style.opacity = "100%";
+  containerImage[1].style.opacity = "100%";
   valueBMI.style.color = "var(--obesite)";
   textSaran.innerHTML = dataTextSaran["obesitas"];
 }
 
-function kembali() {}
+function kembali() {
+  const containerImage = document.getElementsByClassName(
+    "img-bmi-display-item"
+  );
+  const imageMale = document.getElementById("img-bmi-display-male");
+  const imageFemale = document.getElementById("img-bmi-display-female");
+  imageMale.style.display = "none";
+  imageFemale.style.display = "none";
+  for (let i = 0; i < containerImage.length; i++) {
+    containerImage[i].style.opacity = "30%";
+  }
+}
